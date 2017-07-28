955ac52729b502d0cc07c6bdf8444ad8ac67e763
Joshua Duxbury <btbhq@hotmail.com>
2017-07-28 15:34:22 +0100

    Added Http request to JSON feed

--- app/rooms.parent.component.ts
+++ app/rooms.parent.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
- 
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
  
 @Component({
   selector: 'room-parent',
   template: `
     <p>ROOM.</p>
+    {{results}}
     `
 })
 
-export class RoomParentComponent {
+export class RoomParentComponent implements OnInit {
+   results;
+ 
+  // Inject HttpClient into your component or service.
+  constructor(private http: HttpClient) {}
+ 
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http.get('http://joshduxbury.co.uk/projects/mtg/services/rooms.php').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data;
+    });
+  }
 }
