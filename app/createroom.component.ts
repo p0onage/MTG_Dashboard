dead2aa45e2e70fa3122df7a7c7e20947fe3dab1
Joshua Duxbury <btbhq@hotmail.com>
2017-07-28 16:14:56 +0100

    Added Create Room Component

---
+++ app/createroom.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+ 
+@Component({
+  selector: 'create-room',
+  template: `
+        <div>Create Room</div>
+    `
+})
+
+export class CreateRoomComponent{
+
+}
